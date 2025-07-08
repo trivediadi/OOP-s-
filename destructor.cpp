00b748437277d5b0b9575dce6a1f2bb351f5ae7d
trivediadi <trivediadi667@gmail.com>
2025-07-09 00:16:15 +0530

    inheritance concept

--- destructor.cpp
+++ destructor.cpp
@@ -1,2 +1,34 @@
 #include<iostream>
 using namespace std;
+
+class Student{
+public:
+string name;
+double* cgpaPtr;
+ Student(string name,double cgpa){
+    this->name=name;
+    cgpaPtr = new double;
+    *cgpaPtr=cgpa;
+ }
+ // copy constructor using dynamic memory allocation
+ Student(Student &obj){
+    this->name=obj.name;
+    cgpaPtr=new double; // deep copy (allocating new loc to our cgpaptr)
+    *cgpaPtr= *obj.cgpaPtr;
+ }
+ //destructor
+ ~Student(){
+    cout<<"hi i delete everything";
+    // To free dynamic memory we need to use delete keyword
+    delete cgpaPtr;
+ }
+ void get_info(){
+    cout<<"Name: "<<name<<endl;
+    cout<<"Cgpa: "<<*cgpaPtr<<endl;
+ }
+};
+int main(){
+ Student s1("rahul Kumar",9.2);
+ s1.get_info();
+
+}
